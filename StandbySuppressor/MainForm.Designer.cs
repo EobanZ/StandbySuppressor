f13c7ca58589ff39482b691cf5993c4559556f71
Fabi Laptop <zwick.fabi@gmail.com>
2020-04-05 22:46:15 +0200

    first

---
+++ StandbySuppressor/MainForm.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace StandbySuppressor
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblStatusLabel = new System.Windows.Forms.Label();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.btnToggle = new System.Windows.Forms.Button();
+            this.cmbMethod = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.chbDisplay = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // lblStatusLabel
+            // 
+            this.lblStatusLabel.AutoSize = true;
+            this.lblStatusLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStatusLabel.Location = new System.Drawing.Point(14, 9);
+            this.lblStatusLabel.Name = "lblStatusLabel";
+            this.lblStatusLabel.Size = new System.Drawing.Size(111, 32);
+            this.lblStatusLabel.TabIndex = 0;
+            this.lblStatusLabel.Text = "Status: ";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStatus.ForeColor = System.Drawing.Color.Red;
+            this.lblStatus.Location = new System.Drawing.Point(124, 9);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(53, 32);
+            this.lblStatus.TabIndex = 1;
+            this.lblStatus.Text = "Off";
+            // 
+            // btnToggle
+            // 
+            this.btnToggle.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnToggle.Location = new System.Drawing.Point(89, 155);
+            this.btnToggle.Name = "btnToggle";
+            this.btnToggle.Size = new System.Drawing.Size(114, 44);
+            this.btnToggle.TabIndex = 2;
+            this.btnToggle.Text = "Toggle";
+            this.btnToggle.UseVisualStyleBackColor = true;
+            this.btnToggle.Click += new System.EventHandler(this.btnToggle_Click);
+            // 
+            // cmbMethod
+            // 
+            this.cmbMethod.FormattingEnabled = true;
+            this.cmbMethod.Items.AddRange(new object[] {
+            "PowerAvailabilityRequests",
+            "SetThreadExecutionState"});
+            this.cmbMethod.Location = new System.Drawing.Point(20, 73);
+            this.cmbMethod.Name = "cmbMethod";
+            this.cmbMethod.Size = new System.Drawing.Size(242, 24);
+            this.cmbMethod.TabIndex = 3;
+            this.cmbMethod.SelectedIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Method:";
+            // 
+            // chbDisplay
+            // 
+            this.chbDisplay.AutoSize = true;
+            this.chbDisplay.Location = new System.Drawing.Point(20, 103);
+            this.chbDisplay.Name = "chbDisplay";
+            this.chbDisplay.Size = new System.Drawing.Size(136, 21);
+            this.chbDisplay.TabIndex = 5;
+            this.chbDisplay.Text = "Keep Display On";
+            this.chbDisplay.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(290, 233);
+            this.Controls.Add(this.chbDisplay);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmbMethod);
+            this.Controls.Add(this.btnToggle);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.lblStatusLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.ShowIcon = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Show;
+            this.Text = "StandbySuppressor";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblStatusLabel;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Button btnToggle;
+        private System.Windows.Forms.ComboBox cmbMethod;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox chbDisplay;
+    }
+}
+
---
+++ StandbySuppressor/MainForm.cs
@@ -0,0 +1,298 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+namespace StandbySuppressor
+{
+    public partial class MainForm : Form
+    {
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (StandbySuppressor.isStandbySuppressed == true)
+                StandbySuppressor.EnableStandby();
+        }
+
+        private void btnToggle_Click(object sender, EventArgs e)
+        {
+
+            StandbySuppressor.SuppresionMode mode = (StandbySuppressor.SuppresionMode)this.cmbMethod.SelectedIndex;
+
+            if(StandbySuppressor.isStandbySuppressed != true)
+            {
+                StandbySuppressor.isStandbySuppressed = StandbySuppressor.DisableStandby(mode, this.chbDisplay.Checked);
+            }
+            else
+            {
+                StandbySuppressor.isStandbySuppressed = !StandbySuppressor.EnableStandby();
+            }
+
+
+            if(StandbySuppressor.isStandbySuppressed)
+            {
+                this.lblStatus.Text = "On";
+                this.lblStatus.ForeColor = Color.Green;
+            }
+            else 
+            {
+                this.lblStatus.Text = "Off";
+                this.lblStatus.ForeColor = Color.Red;
+            }
+        }
+
+
+    }
+
+    public class StandbySuppressor
+    {
+
+        #region ApiStuff
+        //Main Source: https://decatec.de/programmierung/c-sharp-windows-standby-unterdruecken/
+
+        private const int POWER_REQUEST_CONTEXT_VERSION = 0;
+        private const int POWER_REQUEST_CONTEXT_SIMPLE_STRING = 0x1;
+        private const int POWER_REQUEST_CONTEXT_DETAILED_STRING = 0x2;
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        internal static extern IntPtr PowerCreateRequest(ref POWER_REQUEST_CONTEXT Context);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        internal static extern bool PowerSetRequest(IntPtr PowerRequestHandle, PowerRequestType RequestType);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        internal static extern bool PowerClearRequest(IntPtr PowerRequestHandle, PowerRequestType RequestType);
+
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
+        internal struct POWER_REQUEST_CONTEXT
+        {
+            public UInt32 Version;
+            public UInt32 Flags;
+            [MarshalAs(UnmanagedType.LPWStr)]
+            public string SimpleReasonString;
+        }
+
+        internal enum PowerRequestType
+        {
+            PowerRequestDisplayRequired = 0,
+            PowerRequestSystemRequired,
+            PowerRequestAwayModeRequired,
+            PowerRequestExecutionRequired
+        }
+
+        // Check if power avaiability requests are supported on the current platform.
+        [DllImport("kernel32", CharSet = CharSet.Ansi, ExactSpelling = true, SetLastError = true, BestFitMapping = false, ThrowOnUnmappableChar = true)]
+        internal static extern IntPtr GetProcAddress(IntPtr hModule, string procName);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Unicode)]
+        internal static extern IntPtr LoadLibrary(string dllToLoad);
+
+        private const uint ES_SYSTEM_REQUIRED = 0x00000001;
+        private const uint ES_DISPLAY_REQUIRED = 0x00000002;
+        private const uint ES_USER_PRESENT = 0x00000004; // Only supported by Windows XP/Windows Server 2003
+        private const uint ES_AWAYMODE_REQUIRED = 0x00000040; // Not supported by Windows XP/Windows Server 2003
+        private const uint ES_CONTINUOUS = 0x80000000;
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        internal static extern uint SetThreadExecutionState(uint esFlags);
+
+        #endregion
+        public static bool isStandbySuppressed = false;
+        public static SuppresionMode usedMethod;
+        public static bool keepScreenOn = false;
+
+        private static IntPtr currentPowerRequest;
+
+        public enum SuppresionMode
+        {
+            PowerAvailabilityRequests,
+            SetThreadExecutionState
+        }
+
+
+        public static bool DisableStandby(SuppresionMode mode = SuppresionMode.PowerAvailabilityRequests, bool keepScreenEnabled = false)
+        {
+            keepScreenOn = keepScreenEnabled;
+            bool result = false;
+            switch (mode)
+            {
+                case SuppresionMode.PowerAvailabilityRequests:
+                    if(PowerAvailabilityRequestsSupported())
+                        result = StandbySuppressor.DisableStandbyPowerAvailability();
+                    break;
+                case SuppresionMode.SetThreadExecutionState:
+                    result = StandbySuppressor.DisableStandbySetThreadExec();
+                    break;
+            }
+
+            if (result == true)
+                usedMethod = mode;
+            else
+                MessageBox.Show("Error disableing standby", "error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            return result;
+        }
+
+        public static bool EnableStandby()
+        {
+            bool result = false;
+
+            if (isStandbySuppressed != true)
+                return result;
+
+            switch (usedMethod)
+            {
+                case SuppresionMode.PowerAvailabilityRequests:
+                    result = EnableStandbyPowerAvailablity();
+                    break;
+                case SuppresionMode.SetThreadExecutionState:
+                    result = EnableStandbySetThreadExec();
+                    break;
+            }
+
+            return result;
+        }
+
+        private static bool DisableStandbyPowerAvailability()
+        {
+            // Clear current power request if there is any.
+            if (currentPowerRequest != IntPtr.Zero)
+            {
+                PowerClearRequest(currentPowerRequest, PowerRequestType.PowerRequestSystemRequired);
+                currentPowerRequest = IntPtr.Zero;
+            }
+
+            // Create new power request.
+            POWER_REQUEST_CONTEXT pContext;
+            pContext.Flags = POWER_REQUEST_CONTEXT_SIMPLE_STRING;
+            pContext.Version = POWER_REQUEST_CONTEXT_VERSION;
+            // This is the reason for standby suppression. It is shown when the command "powercfg -requests" is executed.
+            pContext.SimpleReasonString = "Standby suppressed by StandbySuppressor.exe";
+
+            currentPowerRequest = PowerCreateRequest(ref pContext);
+
+            if (currentPowerRequest == IntPtr.Zero)
+            {
+                // Failed to create power request.
+                var error = Marshal.GetLastWin32Error();
+
+                if (error != 0)
+                    throw new Win32Exception(error);
+            }
+       
+            bool success = PowerSetRequest(currentPowerRequest, PowerRequestType.PowerRequestSystemRequired);
+            if(keepScreenOn)
+                success = PowerSetRequest(currentPowerRequest, PowerRequestType.PowerRequestDisplayRequired);
+
+            if (!success)
+            {
+                // Failed to set power request.
+                currentPowerRequest = IntPtr.Zero;
+                var error = Marshal.GetLastWin32Error();
+
+                if (error != 0)
+                    throw new Win32Exception(error);
+            }
+
+            return success;
+        }
+
+        private static bool DisableStandbySetThreadExec()
+        {
+
+            uint flags = keepScreenOn ? ES_CONTINUOUS | ES_SYSTEM_REQUIRED | ES_DISPLAY_REQUIRED : ES_CONTINUOUS | ES_SYSTEM_REQUIRED;
+
+            uint success = SetThreadExecutionState(flags);
+           
+
+            if (success == 0)
+            {
+                // Failed to suppress standby
+                var error = Marshal.GetLastWin32Error();
+
+                if (error != 0)
+                    throw new Win32Exception(error);
+            }
+
+            return success != 0 ? true : false;
+        }
+
+        private static bool EnableStandbyPowerAvailablity()
+        {
+            // Only try to clear power request if any power request is set.
+            if (currentPowerRequest != IntPtr.Zero)
+            {
+                var success = PowerClearRequest(currentPowerRequest, PowerRequestType.PowerRequestSystemRequired);
+                if(keepScreenOn)
+                    success = PowerClearRequest(currentPowerRequest, PowerRequestType.PowerRequestDisplayRequired);
+
+                if (!success)
+                {
+                    // Failed to clear power request.
+                    currentPowerRequest = IntPtr.Zero;
+                    var error = Marshal.GetLastWin32Error();
+
+                    if (error != 0)
+                        throw new Win32Exception(error);
+                }
+                else
+                {
+                    currentPowerRequest = IntPtr.Zero;
+                }
+                return success;
+            }
+            else
+                return false;
+        }
+
+        private static bool EnableStandbySetThreadExec()
+        {
+            var success = SetThreadExecutionState(ES_CONTINUOUS);
+
+            if (success == 0)
+            {
+                // Failed to enable standby
+                var error = Marshal.GetLastWin32Error();
+
+                if (error != 0)
+                    throw new Win32Exception(error);
+            }
+
+            return success != 0 ? true : false; 
+        }
+
+        private static bool PowerAvailabilityRequestsSupported()
+        {
+            var ptr = LoadLibrary("kernel32.dll");
+            var ptr2 = GetProcAddress(ptr, "PowerSetRequest");
+
+            if (ptr2 == IntPtr.Zero)
+            {
+                // Power availability requests NOT suppoted.  
+                MessageBox.Show("PowerAvailability Method not supported on your system", "Method not supportet",MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+            else
+            {
+                // Power availability requests ARE suppoted.                
+                return true;
+            }
+        }
+    }
+    
+}
